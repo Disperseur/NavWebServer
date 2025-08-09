e2a593f183b6f542cc9815481ca467c16c834620
Antoine Chassaigne <antoine.chssgn@gmail.com>
2025-08-09 12:37:21 +0200

    modif fonctionnement alarme (bips et arret sonnerie auto)

--- scripts/log_streaming.py
+++ scripts/log_streaming.py
@@ -5,7 +5,7 @@ FILE_DUMMY1         = "log.txt"
 FILE_NAV1_TEYCHAN   = "nav1.txt"
 FILE_TEST           = "test.txt"
 
-used_file = FILE_NAV1_TEYCHAN
+used_file = FILE_DUMMY1
 
 port = serial.Serial("COM17", 460800)
 
--- src/navwebserver/Alarm.cpp
+++ src/navwebserver/Alarm.cpp
@@ -1,31 +1,57 @@
 #include "Alarm.h"
 
 
-void buzzerAlarmWatcherEntryPoint(void* arg) {
-    Nmea* bateau = (Nmea*)arg;
-
-    pinMode(BUZZER_PIN, OUTPUT); //buzzer pin
-    digitalWrite(BUZZER_PIN, LOW);
-
-    while(true) {
-        if(bateau->get_depth_alarm() || bateau->get_pressure_alarm()) {
+void buzzer_bip(int n, int p) {
+    /*Runs a bip n times a sequence of p*/
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<p; j++) {
             digitalWrite(BUZZER_PIN, HIGH);
             ThisThread::sleep_for(200);
             digitalWrite(BUZZER_PIN, LOW);
             ThisThread::sleep_for(200);
-        } else {
-            ThisThread::sleep_for(1000);
         }
+        ThisThread::sleep_for(400);
+    }
+    ThisThread::sleep_for(1000);
+}
+
+
+void buzzerAlarmWatcherEntryPoint(void *arg)
+{
+    Nmea *bateau = (Nmea *)arg;
+
+    pinMode(BUZZER_PIN, OUTPUT); // buzzer pin
+    digitalWrite(BUZZER_PIN, LOW);
+
+    bool depth_alarm_has_ringed = false;
+    bool pressure_alarm_has_ringed = false;
+
+    while (true) {
+        if (bateau->get_depth_alarm() && !depth_alarm_has_ringed) {
+            buzzer_bip(3, 2);
+            depth_alarm_has_ringed = true;
+        } else if (bateau->get_pressure_alarm() && !pressure_alarm_has_ringed) {
+            buzzer_bip(3, 3);
+            pressure_alarm_has_ringed = true;
+        } else if (!bateau->get_pressure_alarm() && pressure_alarm_has_ringed) {
+            pressure_alarm_has_ringed = false;
+        } else if (!bateau->get_depth_alarm() && depth_alarm_has_ringed) {
+            depth_alarm_has_ringed = false;
+        }
+        ThisThread::sleep_for(1000);
     }
 }
 
-void depthAlarmThreadEntryPoint(void* arg) {
-    Nmea* bateau = (Nmea*)arg;
+void depthAlarmThreadEntryPoint(void *arg)
+{
+    Nmea *bateau = (Nmea *)arg;
 
     float old_depth = bateau->get_water_depthMeters();
 
-    while(true) {
-        if( (bateau->get_water_depthMeters() < 2.0) && (old_depth > 2.0) ) {
+    while (true)
+    {
+        if ((bateau->get_water_depthMeters() < 2.0) && (old_depth > 2.0))
+        {
             bateau->set_depth_alarm(true);
         }
 
@@ -35,36 +61,41 @@ void depthAlarmThreadEntryPoint(void* arg) {
     }
 }
 
-void pressureAlarmThreadEntryPoint(void* arg) {
-  Nmea* bateau = (Nmea*)arg;
+void pressureAlarmThreadEntryPoint(void *arg)
+{
+    Nmea *bateau = (Nmea *)arg;
 
-  Adafruit_BME280 bme; // I2C
+    Adafruit_BME280 bme; // I2C
 
-  int status = bme.begin(0x76, &Wire1);
-  if (!status) {
-      while (1) {
-          Serial1.println("Erreur d'initialisation du bme");
-          delay(10);
-      }
-  }
-  
-  float oldPress = bme.readPressure(); // en Pa
-  bateau->set_pressure_alarm(false);
+    int status = bme.begin(0x76, &Wire1);
+    if (!status)
+    {
+        while (1)
+        {
+            Serial1.println("Erreur d'initialisation du bme");
+            delay(10);
+        }
+    }
 
-  Serial1.println("[ALARM] Service started.");
+    float oldPress = bme.readPressure(); // en Pa
+    bateau->set_pressure_alarm(false);
 
-  while(true) {
+    Serial1.println("[ALARM] Service started.");
+
+    while (true)
+    {
 #ifdef DEBUG_ALARM
-    Serial1.print("[ALARME] Temperature mesuree : ");
-    Serial1.print(bme.readPressure());
-    Serial1.println(" Pa");
+        Serial1.print("[ALARME] Temperature mesuree : ");
+        Serial1.print(bme.readPressure());
+        Serial1.println(" Pa");
 #endif
 
-    if( (oldPress - bme.readPressure()) >= 200.0) { //2hPa
-      bateau->set_pressure_alarm(true);
-    }
-    oldPress = bme.readPressure();
+        if ((oldPress - bme.readPressure()) >= 200.0)
+        { // 2hPa
+            bateau->set_pressure_alarm(true);
+        }
+        oldPress = bme.readPressure();
 
-    ThisThread::sleep_for(600000);
-  }
+        ThisThread::sleep_for(600000);
+    }
 }
